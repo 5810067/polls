1f3b518db96316de29d1db44140d479c181a4780
a5810067 <s5801012610067@email.kmutnb.ac.th>
2017-02-21 15:20:58 +0000

    add comment

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from .models import Choice, Question
-
+# ewqeqfagfhgfhgsehgfjhjhteyhgktreyjyhfryeyfhgjtyygrtyefjhfrytjfhgjtyrdyjhjfdyryjfhjfhtf
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
